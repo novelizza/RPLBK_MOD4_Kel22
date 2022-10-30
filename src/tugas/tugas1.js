31a08b48797eae4aafd9071759870c0d5f16572a
paquitapt11 <64179765+paquitapt11@users.noreply.github.com>
2022-10-30 22:00:57 +0700

    Create tugas1.js

---
+++ src/tugas/tugas1.js
@@ -0,0 +1,55 @@
+import React from "react";
+import axios from "axios";
+import { RootContext } from "../App";
+
+function Tugas() {
+  const [state, setState] = React.useState("");
+  const [state2, setState2] = React.useState([]);
+  const [ubah, setUbah] = React.useState(1);
+
+  React.useEffect(() => {
+    async function restData() {
+      const getData = await axios
+        .get("https://jakpost.vercel.app/api/category/indonesia")
+        .then((data) => setState2(data.data.posts))
+        .catch((er) => console.log(er.response));
+
+      return getData;
+    }
+    if (ubah > 1) {
+      restData();
+    } else {
+      setTimeout(setUbah(2), 10000);
+    }
+  }, [ubah]);
+
+  return (
+    <div
+      style={{
+        flex: 1,
+        flexDirection: "col",
+        backgroundColor: "#22a",
+        width: "50%",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "#bb1",
+          height: 250,
+          borderRadius: 25,
+          paddingLeft: 5,
+        }}
+      >
+        <h1>Tugas Use State</h1>
+        <input
+          onChange={(data) => setState(data.target.value)}
+          placeholder="Isi teks disini..."
+          style={{ width: "50%" }}
+        />
+        <h1>{state}</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Tugas;
